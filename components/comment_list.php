12627c578126cb4e5c8602e0b3d593cb795a9a05
João Almeida <joaoalmeida11@ua.pt>
2023-05-08 11:17:39 +0100

    refactor: removed arrays from comment_list.php

--- components/comment_list.php
+++ components/comment_list.php
@@ -2,6 +2,13 @@
     <?php
 
     session_start();
+
+    // to console.log to the JavaScript console
+    function debugToConsole($msg)
+    {
+        echo "<script>console.log(" . json_encode($msg) . ")</script>";
+    }
+
     // INSERE AQUI O ID DA TUA EQUIPA
     $_SESSION["userId"] = "PHP Masters"; // TODO: make this dynamic
     if (isset($_SESSION['userId'])) {
@@ -29,18 +36,46 @@
             if (mysqli_stmt_execute($stmt)) {
                 /* bind result variables */
                 mysqli_stmt_bind_result($stmt, $id_messages, $message, $time, $recetor, $emissor);
-                // echo "<ul>";
-
-                $messages_list = array();
                 /* fetch values */
                 while (mysqli_stmt_fetch($stmt)) {
-                    array_push($messages_list, array(
-                        'id' => $id_messages,
-                        'message' => $message,
-                        'time' => $time,
-                        'recetor' => $recetor,
-                        'emissor' => $emissor
-                    ));
+                    if ($emissor == $_SESSION["userId"]) {
+                        // message right
+                        echo
+                        '
+                    <div class="row no-gutters">
+        <div class="col-sm-9 offset-sm-3">
+            <div class="chat-bubble--right"><div class="message__list">
+            <p class="message__author-name">' . htmlspecialchars($emissor) . '</p>
+                <p class="chat-bubble">' . htmlspecialchars($message) . '</p>
+                <p class="message__author-name">' . htmlspecialchars($time) . '</p>
+            </div>
+            </div>
+        </div>
+    </div>
+                ';
+                    } else if ($recetor == $_SESSION["userId"]) {
+                        // message left
+                        echo
+                        '
+                    <div class="row no-gutters">
+        <div class="col-sm-6">
+            <div class="chat-bubble--left">
+                <div class="message">
+                    <div class="message__avatar">
+                        <img class="profile-image" src="./assets/cat_photo_01.png" />
+                    </div>
+                    <div class="message__list">
+                        <p class="message__author-name">' . htmlspecialchars($emissor) . '</p>
+                   <p class="chat-bubble">' . htmlspecialchars($message) . '</p>
+                   <p class="message__author-name">' . htmlspecialchars($time) .
+                        '</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+                ';
+                    }
                 }
             } else {
                 // Action on error
@@ -54,86 +89,5 @@
         /* close connection */
         mysqli_close($link);
 
-        // var_dump($messages_list);
-        function debugToConsole($msg)
-        {
-            echo "<script>console.log(" . json_encode($msg) . ")</script>";
-        }
-        // debugToConsole($messages_list);
-
-        $messages_list_formatted = array();
-        foreach ($messages_list as $single_message) {
-            $found_match = false;
-            $last_index = count($messages_list_formatted) - 1;
-
-            if ($last_index >= 0 && $messages_list_formatted[$last_index]['recetor'] === $single_message['recetor'] && $messages_list_formatted[$last_index]['emissor'] === $single_message['emissor']) {
-                array_push($messages_list_formatted[$last_index]['messages_list_same_person'], $single_message['message']);
-                $found_match = true;
-            }
-
-            if (!$found_match) {
-                array_push($messages_list_formatted, array(
-                    'id' => $single_message['id'],
-                    'time' => $single_message['time'],
-                    'recetor' => $single_message['recetor'],
-                    'emissor' => $single_message['emissor'],
-                    'messages_list_same_person' => array($single_message['message'])
-                ));
-            }
-        }
-
-        debugToConsole($messages_list_formatted);
-
-        foreach ($messages_list_formatted as $single_message_list) {
-            if ($single_message_list['emissor'] == $_SESSION["userId"]) {
-                // message right
-                echo
-                '
-                    <div class="row no-gutters">
-        <div class="col-sm-9 offset-sm-3">
-            <div class="chat-bubble--right"><div class="message__list">
-            <p class="message__author-name">' . htmlspecialchars($single_message_list['emissor']) . '</p>
-            ';
-                foreach ($single_message_list['messages_list_same_person'] as $single_message_value) {
-                    echo ' <p class="chat-bubble">' . htmlspecialchars($single_message_value) . '</p>';
-                }
-                echo
-                '
-                <p class="message__author-name">' . htmlspecialchars($single_message_list['time']) . '</p>
-            </div>
-            </div>
-        </div>
-    </div>
-                ';
-            } else if ($single_message_list['recetor'] == $_SESSION["userId"]) {
-                // message left
-
-                echo
-                '
-                    <div class="row no-gutters">
-        <div class="col-sm-6">
-            <div class="chat-bubble--left">
-                <div class="message">
-                    <div class="message__avatar">
-                        <img class="profile-image" src="./assets/cat_photo_01.png" />
-                    </div>
-                    <div class="message__list">
-                        <p class="message__author-name">' . htmlspecialchars($single_message_list['emissor']) . '</p>';
-
-                foreach ($single_message_list['messages_list_same_person'] as $single_message_value) {
-                    echo ' <p class="chat-bubble">' . htmlspecialchars($single_message_value) . '</p>';
-                }
-
-                echo
-                '
-                   <p class="message__author-name">' . htmlspecialchars($single_message_list['time']) . '</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-                ';
-            }
-        }
     }
     ?>
